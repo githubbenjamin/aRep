651c97e37582ec533b1690037d17f94e5c5eeb50
githubbenjamin <yaoze189@yeah.net>
2017-01-05 15:57:41 +0800

    add commands I used while gcc

--- test_lua/test_lua_cmds.sh
+++ test_lua/test_lua_cmds.sh
@@ -1,8 +1,9 @@
 # build so file
 INSTALL_DIR="/usr/local"
 BUILD_FLAGS="-O2 -dynamiclib -undefined dynamic_lookup -I $INSTALL_DIR/include -L $INSTALL_DIR/lib"
-gcc $BUILD_FLAGS -o "$INSTALL_DIR/lib/lua/$LUAV/socket/core.dylib" \
-src/{auxiliar.c,buffer.c,except.c,inet.c,io.c,luasocket.c,options.c,select.c,tcp.c,timeout.c,udp.c,usocket.c}
+#gcc $BUILD_FLAGS -o "$INSTALL_DIR/lib/lua/$LUAV/socket/core.dylib" \
+#src/{auxiliar.c,buffer.c,except.c,inet.c,io.c,luasocket.c,options.c,select.c,tcp.c,timeout.c,udp.c,usocket.c}
+gcc $BUILD_FLAGS -o "myLib01.so" myLib01.cp
 
 # install luasocket
 git clone -b unstable git://github.com/diegonehab/luasocket.git

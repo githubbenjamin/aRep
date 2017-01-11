9e188d956c261bfde172a9446b5eb0ea92d20414
githubbenjamin <yaoze189@yeah.net>
2017-01-11 14:48:49 +0800

    add cjson test

--- test_lua/script.lua
+++ test_lua/script.lua
@@ -17,10 +17,30 @@ print(rtn)
 
 local host, port = "127.0.0.1", 2942
 local ss = assert(socket.tcp())
-print("socket tcp initial success")
 ss:bind(host, port)
 ss:listen(3)
+--[[
 client = ss:accept()
 client:send("get connect ok\n")
 rc = client:receive("*l")
 print("receive:"..rc)
+]]
+
+-- test the cjson
+local json = cjson
+local json_string = "{ \"name\":\"xiaoming\",\"age\":\"3\" }"
+local tab = json.decode(json_string)
+--print("tab->"..table.concat(tab, ",", 0,-1))
+
+t2 = { ["first"]="first value", ["second"]="second value",["third"]="third value" }
+t3 = { t2, {["a1"]="zz",["a2"]="nn"} }
+--table.sort(t2)
+for ii,vv in pairs(t3) do
+for i, v in pairs(vv) do
+print("tab:"..i.."->"..v)
+end
+end
+--for i,v in ipairs(a) do print(i .. "->" ..v) end
+--table.(tab, function(i, v) print(i.."->"..v)end)
+json_string2 = json.encode(t3)
+print("json string->"..json_string2)

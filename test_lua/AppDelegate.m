6aefd49777b3b0938a7667a896b454efa877f896
githubbenjamin <yaoze189@yeah.net>
2017-01-13 15:01:40 +0800

    load lib from source for lua

--- test_lua/AppDelegate.m
+++ test_lua/AppDelegate.m
@@ -3,6 +3,9 @@
 #include "lua.h"
 #include "lauxlib.h"
 #include "lualib.h"
+#include "lua_cjson.h"
+
+
 
 @interface AppDelegate ()
 
@@ -10,6 +13,13 @@
 @end
 
 @implementation AppDelegate
+
+static int socketManneger(lua_State *L)
+{
+    /* load lua socket */
+    luaopen_socket_core(L);
+    return 1;
+}
 
 lua_State* L;
 static int average(lua_State *L)
@@ -35,15 +45,34 @@ static int average(lua_State *L)
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    [self test];
+    NSMutableArray* ma = [NSMutableArray new];
+    [ma lastObject];
+    [ma removeLastObject];
     
     L = luaL_newstate();
     
     /* 载入Lua基本库 */
     luaL_openlibs(L);
+    
+//    lua_register(L, "socketInstance", socketManneger);
+//     luaL_newlib(L, mathlib);
+//    luaL_requiref(L, "socket", luaopen_socket_core, 1);
+//
+    luaopen_socket_core(L);
+    lua_pushvalue(L, -1);  /* copy of module */
+    lua_setglobal(L, "socket");  /* _G[modname] = module */
+    //lua_pop(L, 1);
+    
+    luaopen_cjson(L);
     /* 注册函数 */
     lua_register(L, "average", average);
     /* 运行脚本 */
-    luaL_dofile(L, "avg.lua");
+    int rst = luaL_dofile(L, "main.lua");
+    if (rst != LUA_OK) {
+        //CallInfo *ci = L->ci;
+        printf("error\n");//ci->u.c.ctx);
+    }
     /* 清除Lua */
     lua_close(L);
     
